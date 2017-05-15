9002f57a243bb847aaa8e9137a9c57a29f1d902c
Gino <nico_rodri94@hotmail.com>
2017-05-15 17:29:43 -0300

    lo ultimo

--- src/Arbol/ArbolitoBinario.java
+++ src/Arbol/ArbolitoBinario.java
@@ -19,125 +19,41 @@ public class ArbolitoBinario<E> implements ArbolBinario<E> {
 	 public Iterable<Position<E>> children(Position<E> v) throws InvalidPositionException
 	 	{ }
 	/* retorna si el nodo es interior */
-	 public boolean 
+	 public boolean isExternal(Position<E> v) throws InvalidPositionException{
+	  BTnode<E> nodo = checkPosition( v );
+	 return nodo.getHijos().isEmpty();
+	 }
 	
 	 /* retorna el hijo derecho de un nodo*/
 	 public Position<E> right(Position <E> a) 
 			 throws InvalidPositionException, BoundaryInvalidPosition{
-		 BTnodo<E> aux=checkposition(a);
-		 Position<E> rightpos=auz.getRigth();
+		 BTnode<E> aux=checkPosition(a);
+		 Position<E> rightpos=aux.getRigth();
 		 if(rigthpos==null) throw new BoundaryViolationException("No hijo derecho");
 		 return rigthpos;}
 	
 	 /*retorna si tiene nodo derecho*/
 	public boolean hasRight(Position<E> a) throws InvalidPositionException{
-		 BTnodo<E> aux=checkposition(a);
+		 BTnodo<E> aux=checkPosition(a);
 		 Position<E> rightpos=auz.getRigth();
 		 return (rigthpos!=null);}
 	
-	public int size() {
-		return size;
+	/* remueve un nodo con cero o mas hijos*/
+	public E remove(Position<E> v) throws InvalidPositionException{
+		BTnode<E> nodo=checkPosition(v);
+		BTnode<E> leftpos=nodo.getLeft();
+		BTnode<E> rigthpos=nodo.getRigth();
+		if(leftpos!=null && rigthpos!=null) throw InvalidPositionException("no se puede remover nod con dos hijos");
+		BTnode<E> ww;
+		if()
+		
+		
+		
 	}
 	
-	public Position<E> left(Position<E> v) throws InvalidPositionException, BoundaryViolationException {
-		BTnode<E> nod = checkPosition(v);
-		
-		if (nod.getLeft()==null) {
-			throw new BoundaryViolationException("El nodo no tiene hijo izquierdo.");
-		}
-		else {
-			return nod.getLeft();
-		}
-	}
 	
-	public BTnode<E> checkPosition(Position<E> v) throws InvalidPositionException {
-		if (v==null) {
-			throw new InvalidPositionException("El nodo pasado por parametro es nulo.");
-		}
-		
-		try {
-			return (BTnode<E>) v;
-		}
-		catch (ClassCastException e) {
-			throw new InvalidPositionException("El nodo pasado por parametro no es un nodo arbol.");
-		}
-	}
 	
-	public boolean hasLeft(Position<E> v) throws InvalidPositionException {
-		BTnode<E> nod = checkPosition(v);
-		
-		if (nod.getLeft()==null) {
-			return false;
-		}
-		else {
-			return true;
-		}
-	}
 	
-	public Position<E> addLeft(Position<E> v, E r) throws InvalidOperationException, InvalidPositionException {
-		BTnode<E> nod = checkPosition(v);
-		BTnode<E> nuevonod = new BTnode<E>(r, null, null, null);
-		
-		if (nod.getLeft()!=null) {
-			nod.setLeft(nuevonod);
-		}
-		else {
-			throw new InvalidOperationException("El nodo ya tiene hijo izquierdo.");
-		}
-		
-		size++;
-		return nuevonod;
-	}
-	
-	public Position<E> parent(Position<E> v) throws InvalidPositionException, BoundaryViolationException {
-		BTnode<E> nod = checkPosition(v);
-		
-		if (nod==root) {
-			throw new BoundaryViolationException("El nodo pasado por parametro es la raiz.");
-		}
-		else {
-			return nod.getParent();
-		}
-	}
-	
-	public boolean isRoot(Position<E> v) throws InvalidPositionException {
-		BTnode<E> nod = checkPosition(v);
-		
-		return nod==root;
-	}
-	
-	public boolean isInternal(Position<E> v) throws InvalidPositionException {
-		BTnode<E> nod = checkPosition(v);
-		
-		return nod.getLeft()!=null || nod.getRigth()!=null;
-	}
-	
-	public E replace(Position<E> v, E e) throws InvalidPositionException {
-		BTnode<E> nod = checkPosition(v);
-		
-		E antiguo = nod.element();
-		nod.setElement(e);
-		
-		return antiguo;
-	}
-	
-	public void attach(Position<E> v, ArbolBinario<E> T1, ArbolBinario<E> T2) throws InvalidPositionException {
-		BTnode<E> nod = checkPosition(v);
-		
-		if (!(nod.getLeft()==null && nod.getRigth()==null)) {
-			throw new InvalidPositionException("El nodo pasado por parametro no es hoja.");
-		}
-		
-		try {
-			BTnode<E> root1=checkPosition(T1.root());
-			BTnode<E> root2=checkPosition(T2.root());
-			nod.setLeft(root1);
-			nod.setRigth(root2);
-		}
-		catch (EmptyTreeException e) {
-			throw new InvalidPositionException("El arbol pasado por parametro esta vacio.");
-		}
-	}
 	
 	
 	
